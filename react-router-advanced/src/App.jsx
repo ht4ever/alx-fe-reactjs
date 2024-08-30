dd3138163faf236f9d35d695cb3421bf4079d912
touati <hishamtouati@gmail.com>
2024-08-30 19:11:55 +0100

    new project

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
 import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
 import { AuthProvider } from './context/AuthContext';
@@ -14,11 +15,14 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/blog/1">Blog Post 1</Link></li>
+            <li><Link to="/blog/2">Blog Post 2</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile/*" element={<ProtectedRoute element={<Profile />} />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,16 @@
+// src/components/BlogPost.jsx
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const BlogPost = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h2>Blog Post</h2>
+      <p>Displaying content for blog post ID: {id}</p>
+    </div>
+  );
+};
+
+export default BlogPost;
