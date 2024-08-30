eaf2a72774dfb17ebb253e73d690c1d8029515b8
touati <hishamtouati@gmail.com>
2024-08-30 18:53:10 +0100

    new fixing

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './FormikForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
