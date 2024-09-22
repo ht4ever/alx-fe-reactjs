a36e8449f98854c4efb49fe7234c8e02622179a4
touati <hishamtouati@gmail.com>
2024-09-22 01:27:45 +0100

    1 project

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,56 @@
+// components/Search.jsx
+import React, { useState } from 'react';
+import { fetchUserData } from '../services/githubService';
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!searchTerm) return;
+
+    setLoading(true);
+    setError('');
+    try {
+      const data = await fetchUserData(searchTerm);
+      setUserData(data);
+    } catch (err) {
+      setError('Looks like we can’t find the user.');
+      setUserData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter GitHub username"
+          required
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {userData && (
+        <div>
+          <img src={userData.avatar_url} alt="User Avatar" width="100" />
+          <h3>{userData.name || userData.login}</h3>
+          <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+            Visit GitHub Profile
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
